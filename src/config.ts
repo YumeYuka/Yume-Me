1a9655917d58153ff2baa6b172aa341e5cad724c
YumeYuka <Miao@YumeYuka.plus>
2025-09-29 02:49:15 +0800

    chore(banner): remove top banner image

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,11 @@ export const siteConfig: SiteConfig = {
     subtitle: 'YumeYuka Blog of Dreams',
     lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
     themeColor: {
-        hue: 305,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+        hue: 160,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
         fixed: false,     // Hide the theme color picker for visitors
     },
     banner: {
-        enable: true,
+        enable: false,
         src: 'assets/images/Wallpaper2.jpg',   // Relative to the /src directory. Relative to the /public directory if
         // it starts with '/'
         position: 'top',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
